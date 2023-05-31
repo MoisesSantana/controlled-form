255b0a63b613a79c655ea44865a1958e3136a88c
Moises Santana <moisaant@gmail.com>
2023-05-31 02:53:04 -0300

    refactor: :recycle: Update tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
 function App() {
   return (
     <main>
+      <header>
+        <h1>Welcome</h1>
+        <p>
+          Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu
+          ad litora torquent.Quem num gosta di mim que vai caçá sua turmis!
+          Quem manda na minha terra sou euzis!Nec orci ornare consequat.
+          Praesent lacinia ultrices consectetur. Sed non ipsum felis.
+        </p>
+      </header>
       <form>
-        <header>
-          <h1>Welcome</h1>
-          <p>
-            Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu
-            ad litora torquent.Quem num gosta di mim que vai caçá sua turmis!
-            Quem manda na minha terra sou euzis!Nec orci ornare consequat.
-            Praesent lacinia ultrices consectetur. Sed non ipsum felis.
-          </p>
-        </header>
-        <h2>Login</h2>
-        <div>
+        <img src="/logo.jpg" alt="lorem ipsum company" className="logo" />
+        <div className="input-container">
+          <img src="/user.svg" alt="user" />
           <input type="text" id="email" placeholder="Username" />
-          <input type="text" id="pass" placeholder="******" />
+        </div>
+        <div className="input-container">
+          <img src="/pass.svg" alt="password" />
+          <input type="password" id="pass" placeholder="******" />
         </div>
         <button>Login</button>
-        <div>
+        <footer>
           <div>
             <input type="checkbox" id="remember" />
             <label htmlFor="remember">Remember me</label>
           </div>
-          <a href="https://betrybe.com">Esqueci minha senha</a>
-        </div>
+          <a href="https://betrybe.com">Forget password</a>
+        </footer>
       </form>
     </main>
   );
